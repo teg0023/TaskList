6fb03f8903ea987a31f42ae84d97a10fc9bbf9af
teg0023 <teg0023@auburn.edu>
2024-06-12 16:16:46 -0500

    Initial Commit

---
+++ TaskList/TaskListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskListApp.swift
+//  TaskList
+//
+//  Created by user254330 on 6/12/24.
+//
+
+import SwiftUI
+
+@main
+struct TaskListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
